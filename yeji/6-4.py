d3495638e56be204fb3fc9b2e8ec927ffa978a8b
yunyezl <69361613+yunyezl@users.noreply.github.com>
2020-10-08 12:55:36 +0900

    Update 6-4.py

--- yeji/6-4.py
+++ yeji/6-4.py
@@ -2,9 +2,13 @@ n, k = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
+
 for i in range(k):
     minA = A.index(min(A)) 
     maxB = B.index(max(B))
-    A[minA], B[maxB] = B[maxB], A[minA]
+    if A[minA] < B[maxB]:
+        A[minA], B[maxB] = B[maxB], A[minA]
+    else:
+        break
 
 print(sum(A))
