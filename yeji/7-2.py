25eea86b098cae94da72e710a28f98f77910551f
yunyezl <69361613+yunyezl@users.noreply.github.com>
2020-11-04 20:13:17 +0900

    Create 7-2.py

---
+++ yeji/7-2.py
@@ -0,0 +1,26 @@
+global h
+h = 0
+
+def search(array, target, start, end): 
+    global h
+    slideDduckList = []
+
+    for i in range(end+1):
+        if (array[i] - h > 0):
+            slideDduck = array[i] - h
+        else:
+            slideDduck = 0
+        slideDduckList.append(slideDduck)
+
+    if sum(slideDduckList) == target :
+        print(h)
+        return -1
+    else:
+        h = h + 1
+        search(array, target, start, end) 
+
+n, m = map(int, input().split())
+dduck = list(map(int, input().split()))
+
+dduck.sort()
+search(dduck, m, 0, n - 1)
