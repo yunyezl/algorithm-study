14c58b9042b596f8c4fe07ba3f9e595f019c304d
yunyezl <69361613+yunyezl@users.noreply.github.com>
2020-11-04 20:41:40 +0900

    Update 7-2.py

--- yeji/7-2.py
+++ yeji/7-2.py
@@ -24,3 +24,24 @@
 
 # dduck.sort()
 # search(dduck, m, 0, n - 1)
+
+n, m = map(int, input().split())
+dduck = list(map(int, input().split()))
+
+start = 0
+end = dduck[n-1]
+
+result = 0
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for i in dduck:
+        if i > mid:
+            total += i - mid
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
